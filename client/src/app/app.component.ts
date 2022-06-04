2118e8d5eef671b71f95c3fc4556e1c7eca04a37
Anushree Desai <anushree.desai@maersk.com>
2022-06-04 12:13:12 +0530

    Commit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
   }
 
   getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
+    this.http.get('https://localhost:5001/api/Users').subscribe({
       next: response => this.users.response,
       error: error => console.log(error)
   })
