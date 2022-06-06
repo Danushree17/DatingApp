a4ea8aaa26d6147c2c13a1b697ae23287d09b584
Anushree Desai <anushree.desai@maersk.com>
2022-06-06 20:18:19 +0530

    End of section

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -16,17 +16,17 @@ export class AppComponent implements OnInit {
     this.getUsers();
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/Users').subscribe({
-      next: response => this.users.response,
-      error: error => console.log(error)
-  })
-  }
   // getUsers(){
-  //   this.http.get('https://localhost:5001/api/users').subscribe(response => {
-  //     this.users = response;
-  //   }, error => {
-  //     console.log(error);
-  //   })
+  //   this.http.get('https://localhost:5001/api/Users').subscribe({
+  //     next: response => this.users.response,
+  //     error: error => console.log(error)
+  // })
   // }
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
