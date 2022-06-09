44a045548efbc47520d35aec39d92b76e4328ff3
Anushree Desai <anushree.desai@maersk.com>
2022-06-09 16:37:17 +0530

    End Of Section DTO

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,13 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 using API.Data;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
-//using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[Controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -21,12 +19,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();                
         }
         
-        [HttpGet("{id}")]
+        [Authorize]
+        [HttpGet("{id}")]        
         public async Task<ActionResult<AppUser>> GetUsers(int id)
         {
             return await _context.Users.FindAsync(id);                
